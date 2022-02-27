00b98f4f2ad48830cd384951aa3507d58775682d
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-02-27 23:28:15 +0530

    adding welcome file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class program
+    {
+        public void Main(string[] args)
+        {
+            //LenthPblm LenthPblm = new LenthPblm();
+            //LenthPblm.CalculatingLength();
+
+
+            //Equality Equality = new Equality();
+            //Equality.CheckEquality();
+
+            Comparision Comparision = new Comparision();
+            Comparision.ComparingTwoLine();
+        }
+    }
+}
